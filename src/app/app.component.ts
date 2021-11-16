774f61d029c67039db3c7a8782c98a7e0843e085
Ayoub Alfurjani <58040995+ZeroGDrive@users.noreply.github.com>
2021-11-16 09:44:35 +0200

    Pwa update event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-video-player';
+
+  constructor(private readonly _swUpdate: SwUpdate) {}
+
+  ngOnInit(): void {
+    if (this._swUpdate.isEnabled) {
+      this._swUpdate.versionUpdates
+        .pipe(filter((e) => e.type === 'VERSION_READY'))
+        .subscribe(() => {
+          if (confirm('New version available. Load New Version?')) {
+            window.location.reload();
+          }
+        });
+    }
+  }
 }
